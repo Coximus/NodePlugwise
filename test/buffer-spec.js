bf1ef1abf39a4136747b0fe42e0aa47f5e73674a
cox <coximusmaximus@googlemail.com>
2016-01-24 22:41:46 +0000

    Added buffer.js and tests

--- test/buffer-spec.js
+++ test/buffer-spec.js
@@ -3,7 +3,7 @@ var assert = require('assert'),
     Buffer = require('../buffer.js');
 
 describe('Buffer', function() {
-    it('should return an array on messages when emmiting a messages event', function(next) {
+    it('should return an array of messages when emmiting a messages event', function(next) {
         var buffer = new Buffer();
         buffer.on('messages', function(data) {
             assert(Array.isArray(data));
@@ -37,6 +37,91 @@ describe('Buffer', function() {
             assert.equal(data.length, 2);
             next();
         });
-        buffer.store("first message7second message7partial mes...");
+        buffer.store("first message\nsecond message\npartial mes...");
+    });
+
+    it('should have stored any incomplete messages from previously processing', function(next) {
+        var buffer = new Buffer();
+        var count = 0;
+        buffer.on('messages', function(data) {
+            if (count === 0) {
+                assert.equal(data.length, 2);
+                assert.equal(data[0], 'first message');
+                assert.equal(data[1], 'second message');
+                count++;    
+                return;
+            }
+            assert.equal(data.length, 2);
+            assert.equal(data[0], 'partial message');
+            assert.equal(data[1], 'another message');
+            next();
+        });
+        buffer.store("first message\nsecond message\npartial mes");
+        buffer.store("sage\nanother message\n");
+    });
+
+    it('should support custom footers', function(next) {
+        var buffer = new Buffer(),
+            count = 0;
+            
+        buffer.setPatternEnd('~');
+        buffer.on('messages', function(data) {
+            if (count === 0) {
+                assert.equal(data.length, 2);
+                assert.equal(data[0], 'first message');
+                assert.equal(data[1], 'second message');
+                count++;    
+                return;
+            }
+            assert.equal(data.length, 2);
+            assert.equal(data[0], 'partial message');
+            assert.equal(data[1], 'another message');
+            next();
+        });
+        buffer.store("first message~second message~partial mes");
+        buffer.store("sage~another message~");
+    });
+
+    it('should support custom footer patterns', function(next) {
+        var buffer = new Buffer(),
+            count = 0;
+            
+        buffer.setPatternEnd('(?:~|,)');
+        buffer.on('messages', function(data) {
+            if (count === 0) {
+                assert.equal(data.length, 2);
+                assert.equal(data[0], 'first message');
+                assert.equal(data[1], 'second message');
+                count++;    
+                return;
+            }
+            assert.equal(data.length, 2);
+            assert.equal(data[0], 'partial message');
+            assert.equal(data[1], 'another message');
+            next();
+        });
+        buffer.store("first message~second message,partial mes");
+        buffer.store("sage~another message~");
+    });
+
+    it('should support custom headers', function(next) {
+        var buffer = new Buffer(),
+            count = 0;
+            
+        buffer.addPatternStart('~');
+        buffer.on('messages', function(data) {
+            if (count === 0) {
+                assert.equal(data.length, 2);
+                assert.equal(data[0], 'first message');
+                assert.equal(data[1], 'second message');
+                count++;    
+                return;
+            }
+            assert.equal(data.length, 1);
+            assert.equal(data[0], 'partial message');
+            next();
+        });
+        buffer.store("random data~first message\n~second message\n~partial mes");
+        buffer.store("sage\n");
     });
 });
