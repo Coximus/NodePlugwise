ce10f1679b0513025a2f8c6bbc8eb6b0e1e69ac9
cox <coximusmaximus@googlemail.com>
2016-03-10 23:31:17 +0000

    Added test as a reminder of where I am up to

--- test/buffer-processor-spec.js
+++ test/buffer-processor-spec.js
@@ -3,10 +3,14 @@ var assert = require('assert'),
     BufferProcessor = require('../bufferProcessor.js');
 
 describe('Buffer Processor', function() {
-    it('should return null if the message is not valid', function() {
+    it('should return null if the data input is not valid', function() {
         var bufferInput = "Invalid Buffer Data",
             result = BufferProcessor(bufferInput);
 
         assert(!result);
+    });
+
+    it('should return a plugwise message model if the data input is valid', function() {
+        assert(false);
     });
 });
