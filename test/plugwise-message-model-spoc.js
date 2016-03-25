ef3c4bd86bfcdf987692c79d0a41b5b806af620c
paul Alcock <paul.alcock@skybettingandgaming.com>
2016-03-25 22:23:00 +0000

    Converted tabs to spaces

--- test/plugwise-message-model-spoc.js
+++ test/plugwise-message-model-spoc.js
@@ -1,24 +1,24 @@
 var assert = require('assert'),
-	PlugwiseMessageModel = require('../PlugwiseMessageModel');
+    PlugwiseMessageModel = require('../PlugwiseMessageModel');
 
 describe('Plugwise Message Model', function() {
-	describe('isAck', function() {
-		it('should return true if code and parameters signify it is an ACK', function() {
-			var message = new PlugwiseMessageModel({code: "0000", sequenceNo: "0001", parameters: "00C1"});
+    describe('isAck', function() {
+        it('should return true if code and parameters signify it is an ACK', function() {
+            var message = new PlugwiseMessageModel({code: "0000", sequenceNo: "0001", parameters: "00C1"});
 
-			assert(message.isAck());
-		});
+            assert(message.isAck());
+        });
 
-		it('should return false if code and parameters signify it is an ACK', function() {
-			var messages = [
-				{code: "0000", sequenceNo: "0001", parameters: "00E1"},
-				{code: "0001", sequenceNo: "0001", parameters: "00C1"}
-			];
+        it('should return false if code and parameters signify it is an ACK', function() {
+            var messages = [
+                {code: "0000", sequenceNo: "0001", parameters: "00E1"},
+                {code: "0001", sequenceNo: "0001", parameters: "00C1"}
+            ];
 
-			messages.forEach(function(message) {
-				var msg = new PlugwiseMessageModel(message);
-				assert.equal(false, msg.isAck());
-			});
-		});
-	});
+            messages.forEach(function(message) {
+                var msg = new PlugwiseMessageModel(message);
+                assert.equal(false, msg.isAck());
+            });
+        });
+    });
 });
