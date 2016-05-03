cc3ae9791f07687149cfd98556e1d9840552a05f
paul Alcock <paul.alcock@skybettingandgaming.com>
2016-05-03 14:18:05 +0100

    Added tests for returned data

--- PlugwiseTxMessages/Initialise.js
+++ PlugwiseTxMessages/Initialise.js
@@ -1,13 +1,19 @@
 var txMessageModel = require('../TransmissionMessageModel');
 
 var Initialise = function(callback) {
-    // var processMessages = function(error, messages) {
-    //     if (error) {
-    //         callback(console.error(error));
-    //     }
+    var processMessages = function(error, messages) {
+        if (error) {
+            callback(console.error(error));
+        }
 
-    //     callback(null, messages);
-    // };
+        var networkData = {
+            network: messages[0].parameters.substring(0,10),
+            stick: messages[0].parameters.substring(10,16),
+            circlePlus: messages[0].parameters.substring(30,36)
+        }
+
+        callback(null, networkData);
+    };
 
     this.__proto__ = txMessageModel(
         { type: 0, message: "\x30\x30\x30\x41"},
--- test/unit/PlugwiseTxMessages/initialise-spec.js
+++ test/unit/PlugwiseTxMessages/initialise-spec.js
@@ -1,20 +1,131 @@
 var assert = require('assert'),
-    InitialiseMsg = require('../../../PlugwiseTxMessages/Initialise');
+    InitialiseMsg = require('../../../PlugwiseTxMessages/Initialise'),
+    PlugwiseMessage = require('../../../PlugwiseMessageModel'),
+    Promise = require('bluebird');
 
 describe('PlugwiseTxMessage - Initialise', function() {
-    it('should store a message type of 0', function() {
-        var msg = new InitialiseMsg();
+    describe('Message Creatiion', function() {
+        it('should store a message type of 0', function() {
+            var msg = new InitialiseMsg();
 
-        assert.deepEqual(0, msg.type);
+            assert.deepEqual(0, msg.type);
+        });
+
+        it('should set a message of 000A', function() {
+            var msg = new InitialiseMsg();
+
+            assert.equal("\x05\x05\x03\x03000AB43C\x0D\x0A", msg.message);
+        });
     });
 
-    it('should set a message of 000A', function() {
-        var msg = new InitialiseMsg();
+    describe('Obtaining network data', function() {
+        it('should return an object with the network address back through the callback', function(done) {
+            var fixtures = getFixtures(),
+                promises = getPromisedMessages(fixtures);
 
-        assert.equal("\x05\x05\x03\x03000AB43C\x0D\x0A", msg.message);
-    });
+            Promise.all(promises).then(function(networkData){
+                var i;
+                for(i = 0; i < fixtures.length; i++) {
+                    assert.equal(fixtures[i].network, networkData[i].network);
+                }
+                done();
+            }).catch(function(e) {
+                done(e);
+            });
+        });
 
-    it('should return the address object back through the callback', function() {
-        assert(false);
+        it('should return an object with the stick address back through the callback', function(done) {
+            var fixtures = getFixtures(),
+                promises = getPromisedMessages(fixtures);
+
+            Promise.all(promises).then(function(networkData){
+                var i;
+                for(i = 0; i < fixtures.length; i++) {
+                    assert.equal(fixtures[i].stick, networkData[i].stick);
+                }
+                done();
+            }).catch(function(e) {
+                done(e);
+            });
+        });
+
+        it('should return an object with the circlePlus address back through the callback', function(done) {
+            var fixtures = getFixtures(),
+                promises = getPromisedMessages(fixtures);
+
+            Promise.all(promises).then(function(networkData){
+                var i;
+                for(i = 0; i < fixtures.length; i++) {
+                    assert.equal(fixtures[i].circlePlus, networkData[i].circlePlus);
+                }
+                done();
+            }).catch(function(e) {
+                done(e);
+            });
+        });
+
+        it('should return data from the first recption of type 0011', function(done) {
+            var fixtures = getFixtures(),
+                promises = [];
+
+            fixtures.forEach(function(fixture) {
+                promises.push(new Promise(function(resolve, reject){
+                    var receptions = [
+                            new PlugwiseMessage({code: '1234'}),
+                            new PlugwiseMessage({code: '0011', parameters: fixture.rawMessage})
+                        ],
+                        msg = new InitialiseMsg(function(error, networkData) {
+                            if (error) {
+                                console.log('rejecting');
+                                reject('error');
+                            }
+                            resolve(networkData);
+                        });
+
+                    msg.callback(null, receptions);
+                }));
+            });
+
+            Promise.all(promises).then(function(networkData){
+                var i;
+                for(i = 0; i < fixtures.length; i++) {
+                    assert.equal(fixtures[i].circlePlus, networkData[i].circlePlus);
+                }
+                done();
+            }).catch(function(e) {
+                done(e);
+            });
+        });
+
+        function getPromisedMessages(fixtures) {
+            var promises = [];
+
+            fixtures.forEach(function(fixture) {
+                promises.push(new Promise(function(resolve, reject){
+                    var receptions = [new PlugwiseMessage({code: '0011', parameters: fixture.rawMessage})],
+                        msg = new InitialiseMsg(function(error, networkData) {
+                            resolve(networkData);
+                        });
+
+                    msg.callback(null, receptions);
+                }));
+            });
+
+            return promises;
+        }
+
+        function getFixtures(){
+            return [{
+                network: '000D6F0000',
+                stick: '99558D',
+                circlePlus: '768D95',
+                rawMessage: '000D6F000099558D0101480D6F0000768D955B48FF'
+            },{
+                network: '000D6F0001',
+                stick: '99558E',
+                circlePlus: '768D96',
+                rawMessage: '000D6F000199558E0101480D6F0000768D965B48FF'
+            }];
+        }
     });
 });
